d3cc77295269423797097b0adbd1dd0aa5716383
noor <113855980+narahim@users.noreply.github.com>
2026-02-09 12:08:20 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example-1" {
     ami = var.ami_value
     instance_type = var.instance_type_value
-    associate_public_ip_address = true
+}
+
+
+resource "aws_instance" "example-2" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+}
+
+
+resource "aws_instance" "example-3" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
 }
